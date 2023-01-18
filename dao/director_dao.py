10824fdf0d6408ce58f2b5a63f77b5b36ffb0de2
svkote <kotekofly@gmail.com>
2023-01-18 23:52:54 +0300

    director_dao.py and genre_dao.py

---
+++ dao/director_dao.py
@@ -0,0 +1,12 @@
+from model.director import Director
+
+
+class DirectorDao:
+    def __init__(self, session):
+        self.session = session
+
+    def get_all(self):
+        return self.session.query(Director).all()
+
+    def get_one(self, did: int) -> Director:
+        return self.session.query(Director).get(did)
---
+++ dao/genre_dao.py
@@ -0,0 +1,12 @@
+from model.genre import Genre
+
+
+class GenreDao:
+    def __init__(self, session):
+        self.session = session
+
+    def get_all(self):
+        return self.session.query(Genre).all()
+
+    def get_one(self, gid: int) -> Genre:
+        return self.session.query(Genre).get(gid)
